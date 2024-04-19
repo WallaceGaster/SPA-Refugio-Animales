8ff159775544d82eaf0074ab2224442b70723bb7
Naomi <hernadez.romo.n14@gmail.com>
2024-04-18 22:42:52 -0600

    galeria masonry

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { MascotasComponent } from './mascotas/mascotas.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'mascotas', component: MascotasComponent }
+    { path: 'mascotas', component: MascotasComponent },
+    { path: 'galeria', component: GaleriaComponent }
 ];
