9d748fa38aa15de1ce6295dcbf95ca4a04c56d0c
WallaceGaster <saitama.kurosakyy@gmail.com>
2024-04-30 17:11:26 -0600

    Pagina de about modificada y casi terminada

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatDividerModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
