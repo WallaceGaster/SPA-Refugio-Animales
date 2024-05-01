909b0bb7b6b2b235c53e51280bf26846c444e270
JalkemMorales <jalkemmorales@gmail.com>
2024-04-30 22:44:18 -0600

    Localstorage II

--- src/app/shared/mascota.service.ts
+++ src/app/shared/mascota.service.ts
@@ -7,7 +7,7 @@ import { Mascota } from '../mascota';
   providedIn: 'root',
 })
 export class MascotaService {
-  urlAPI: string = 'https://mascotas-1.free.beeceptor.com/todos';
+  urlAPI: string = 'https://mascotas-97.free.beeceptor.com/todos';
   arreglo: Mascota[] = [];
 
   constructor(private http: HttpClient) {}
