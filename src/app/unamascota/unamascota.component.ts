6c573d2ee920351ed2a8f149b6dd610258040eb0
WallaceGaster <saitama.kurosakyy@gmail.com>
2024-05-02 22:28:17 -0600

    Estilos a botones

--- src/app/unamascota/unamascota.component.ts
+++ src/app/unamascota/unamascota.component.ts
@@ -1,20 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Mascota } from '../mascota';
 import { MascotaService } from '../shared/mascota.service';
+import { CitasService } from '../shared/citas.service';
+import { Cita } from '../shared/cita.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-unamascota',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './unamascota.component.html',
   styleUrl: './unamascota.component.css'
 })
-export class UnamascotaComponent {
+export class UnamascotaComponent implements OnInit{
   @Input() mascota!:Mascota;
-  constructor(public mascotaService:MascotaService, public activatedRoute: ActivatedRoute) {
+  constructor(public mascotaService:MascotaService, public activatedRoute: ActivatedRoute, private citasService: CitasService ) {
     this.activatedRoute.params.subscribe(params => {
       this.mascota=mascotaService.getUnaMascota(params['id']);
     })
   }
+  obcita!: Cita[];
+  citas: any[] = [];
+  fechaActual: Date = new Date();
+
+  ngOnInit(): void {
+    this.recuperarCitas();
+  }
+  recuperarCitas(): any[] {
+    this.obcita = this.citasService.getCitas();
+    this.obcita = this.obcita.filter(cita => cita.Mascota.nombre === this.mascota.nombre);
+    return this.obcita; 
+  }  
 }
